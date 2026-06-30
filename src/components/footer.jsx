0853f02f90cb4fbdccdc739a59fcb6e7add1826b
Thyagoemanoell <thyagoemanoel@outlook.com>
2026-06-30 05:50:43 -0300

    atualizacao

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -59,14 +59,14 @@ function Footer() {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
                 {/* EDITAR: email do corretor */}
-                <span>corretor@email.com</span>
+                <span>ayrton_bruno@hotmail.com</span>
               </div>
               <div className="contato-det-item footer-det-item">
                 <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.8">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
                 {/* campo de telefone do corretor */}
-                <span>(98) 98465-5966</span>
+                <span>(98) 98465-596</span>
               </div>
               <div className="contato-det-item footer-det-item">
                 <WaIcon />
@@ -143,18 +143,18 @@ function Footer() {
           </a>
 
           {/* campo do link do Facebook */}
-          <a className="soc-ico" href="https://facebook.com/" target="_blank" rel="noreferrer" aria-label="Facebook">
+          <a className="soc-ico" href="https://www.facebook.com/brunosenaimoveis" target="_blank" rel="noreferrer" aria-label="Facebook">
             <svg fill="currentColor" viewBox="0 0 24 24">
               <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
             </svg>
           </a>
 
           {/* campo do link do LinkedIn */}
-          <a className="soc-ico" href="https://linkedin.com/" target="_blank" rel="noreferrer" aria-label="LinkedIn">
+          {/* <a className="soc-ico" href="https://linkedin.com/" target="_blank" rel="noreferrer" aria-label="LinkedIn">
             <svg fill="currentColor" viewBox="0 0 24 24">
               <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
             </svg>
-          </a>
+          </a> */}
         </div>
       </div>
 
