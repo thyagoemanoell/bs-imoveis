7a4b29a9f4317cff686f443848037d002263ca90
Marksz <marcoaraujo.developer@gmail.com>
2026-06-30 23:31:21 -0300

    fix: ajuste dos nomes dos componentes, de iniciais minúsculas para maiúsculas em todos os arquivos

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/css/estilo.css'
-import Navbar from './components/navbar'
-import Principal from './components/principal'
-import Contato from './components/contato'
-import Footer from './components/footer'
+import Navbar from './components/Navbar'
+import Principal from './components/Principal'
+import Contato from './components/Contato'
+import Footer from './components/Footer'
 
 function App() {
   const [contatoAberto, setContatoAberto] = useState(false);
